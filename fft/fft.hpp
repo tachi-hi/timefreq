b3dbbb16055a9e14d68abc08edc28ec95851a058
Hideyuki Tachibana <tachibana@hil.t.u-tokyo.ac.jp>
2012-06-22 20:57:41 +0900

    changed the place of fft size check

--- fft/fft.hpp
+++ fft/fft.hpp
@@ -14,7 +14,7 @@ class FFT{
  public:
 	FFT(){}
 	~FFT(){}
-	void init(int length);
+	void init(const int length);
 
 	void forward (const double *input, std::complex<double> *output) {fft_(input, output, true); }
 	void inverse (const std::complex<double> *input, double *output) {fft_(input, output, false); }
@@ -28,9 +28,15 @@ class FFT{
 	int length;
 };
 
-void FFT::init(int length)
+void FFT::init(const int length)
 {
 	this->length = length;
+
+	if (length < 2 || (length & (length - 1)) != 0){
+		fprintf(stderr, "Error in fft.hpp. Size of input data is not 2^n");
+		return;
+	}
+
 	for(int i = 0; i < length; i++)
 	{
 		COS.push_back( cos(2. * M_PI * (double)i/length) );
@@ -41,10 +47,6 @@ void FFT::init(int length)
 
 template<class S, class T>
 void FFT::fft_(const S *input, T *output, const bool flag){
-	if (length < 2 || (length & (length - 1)) != 0){
-		fprintf(stderr, "Error in fft.hpp. Size of input data is not 2^n");
-		return;
-	}
 
 	std::complex<double> *alias = typeid(T) != typeid(std::complex<double>)
 		? &(buffer[0])
@@ -61,16 +63,12 @@ void FFT::fft_(const S *input, T *output, const bool flag){
 	for(int len = length; len >= 1; len >>= 1){
 		int angle = 0;
 		int angle_hop = length / len;
-		for(int k = 0; 
-				k < (len >> 1); 
-				++k, angle += angle_hop)
+		for(int k = 0; k < (len >> 1); ++k, angle += angle_hop)
 		{
 			std::complex<double> W( COS[ angle ], sign * SIN[ angle ] );
 			int p = k;
 			int q = k + (len >> 1);
-			for(int j = len; 
-					j <= length; 
-					j += len, p += len, q += len)
+			for(int j = len; j <= length; j += len, p += len, q += len)
 			{
 				t = alias[p] - alias[q];
 				alias[p] += alias[q];
@@ -81,9 +79,7 @@ void FFT::fft_(const S *input, T *output, const bool flag){
 
 	int j = 1;
 	int p = 0;
-	for(int i = 1 ; 
-			i <= length - 1 ; 
-			++i, ++p)
+	for(int i = 1; i <= length - 1; ++i, ++p)
 	{
 		if( i < j ){
 			t = alias[p];
