31d88d27419e2cc7da1e6bb372fa13ecab5a8be0
Hideyuki Tachibana <tachibana@hil.t.u-tokyo.ac.jp>
2014-09-29 17:18:17 +0900

    added chroma

--- cq/cq_sample.cpp
+++ cq/cq_sample.cpp
@@ -6,6 +6,7 @@
 #include<iostream>
 #include<cstdio>
 #include<string>
+#include<vector>
 
 //getopt
 #include<getopt.h>
@@ -25,6 +26,7 @@ int main(int argc, char **argv){
 	short n_nyq = 72; //6 octaves
 	short resol = 5;
 	double Q = 45;
+	bool chroma_flag = false;
 	enum{
 		FORMAT_gnuplot,
 		FORMAT_matrix,
@@ -36,17 +38,17 @@ int main(int argc, char **argv){
 //		int this_option_optind = optind ? optind : 1;
 		int option_index = 0;
 		static struct option long_options[] = {
-			{"help",    0,      0,   'h'},
-			{"input",   1,      0,   'i'},
-			{"srate",   1,      0,   's'},
-			{"fshift",  1,      0,   'f'},
-			{"startfreq",1,     0,   'q'},
-			{"nnyq",    1,      0,   'n'},
-			{"resol",   1,      0,   'r'},
-			{"sigma",   1,      0,   'g'},
-			{"Q",       1,      0,   'Q'},
-			{"Format",  1,      0,   'F'},
-			{"a",       1,      0,   'a'},
+			{"help",    no_argument,            0,   'h'},
+			{"input",   required_argument,      0,   'i'},
+			{"srate",   required_argument,      0,   's'},
+			{"fshift",  required_argument,      0,   'f'},
+			{"startfreq",required_argument,     0,   'q'},
+			{"nnyq",    required_argument,      0,   'n'},
+			{"resol",   required_argument,      0,   'r'},
+			{"sigma",   required_argument,      0,   'g'},
+			{"Q",       required_argument,      0,   'Q'},
+			{"Format",  required_argument,      0,   'F'},
+			{"chroma",  no_argument,            0,   'c'},
 			{0,         0,      0,    0}
 		};
 		int c = getopt_long(argc, argv, "hi:P:s:f:q:n:r:g:Q:", long_options, &option_index);
@@ -65,6 +67,7 @@ int main(int argc, char **argv){
 			cerr << "  --resol\t frequency resolugion" << endl;
 			cerr << "  --Q\t Q" << endl;
 			cerr << "  --Format\t output format; gnuplot (default), matrix, csv." << endl;
+			cerr << "  --chroma\t change output format to chroma vector" << endl;
 			cerr << "(c) 2012 Hideyuki Tachibana, tachi-hi @ github" << endl;
 			exit(1);
 		}
@@ -88,8 +91,9 @@ int main(int argc, char **argv){
 			}
 			break;
 		}
+        case'c':{chroma_flag = true; break;}
 		default:
-			printf("Error: getopt returned character code 0%o \n", c);
+			printf("Error: getopt returned character code %c (0%o) \n", c, c);
 		}
 	}
 	n_nyq = n_nyq * resol;
@@ -120,8 +124,8 @@ int main(int argc, char **argv){
 
 	input->read(frame_length / 2 - frame_shift);
 	double *in = new double [frame_length];
-	double *spec = new double [n_nyq];
-
+    std::vector<double> spec(n_nyq);
+    std::vector<double> chroma(resol * 12);
 	FILE *file = stdout;
 
 	int last_count = 0;
@@ -136,45 +140,50 @@ int main(int argc, char **argv){
 		input->read(frame_shift);
 		input->copy(in, 0, frame_length);
 
-		// transform
-		cq->exec(in, spec);
-
-		if (count % 10 == 0) 
+        // display the progress 
+		if (count % 100 == 0) 
 			fprintf(stderr, "%f[s]\n", (double)count * frame_shift / sampling_rate);
 
+		// transform
+		cq->exec(in, &(spec[0]));
+
+        // chroma
+        if(chroma_flag){
+            for (int i = 0; i < resol * 12; ++i){
+                chroma[i] = 0;
+            }
+            for (int i = 0; i < n_nyq; ++i){
+                chroma[ i % (resol * 12) ] += spec[i];
+            }
+        }
+
+        // alias
+        const std::vector<double> &output = chroma_flag ? chroma : spec;
+        
 		// save
-		if(format_flag == FORMAT_gnuplot)
-		{
-			for(int i = 0; i < n_nyq; ++i)
-			{
-				double tmp1 = (double)count * frame_shift / sampling_rate;
-				double tmp2 = start_freq * pow(2., static_cast<double>(i)/12./resol);
-				fprintf(file, "%f %f %e\n", tmp1, tmp2, spec[i]);
-			}
-		}
-		else if (format_flag == FORMAT_matrix)
-		{
-			for(int i = 0; i < n_nyq; ++i)
-			{
-				fprintf(file, "%f%s", spec[i], i < n_nyq - 1 ? " " : "");
-			}
-		}
-		else if (format_flag == FORMAT_csv)
-		{
-			for(int i = 0; i < n_nyq; ++i)
-			{
-				fprintf(file, "%f%s", spec[i], i < n_nyq - 1 ? "," : "");
-			}
-		}
+		for(unsigned int i = 0; i < output.size(); ++i){		
+		    if(format_flag == FORMAT_gnuplot)
+		    {
+				    const double tmp1 = (double)count * frame_shift / sampling_rate;
+				    const double tmp2 = start_freq * pow(2., static_cast<double>(i)/12./resol);
+				    fprintf(file, "%f %f %e\n", tmp1, tmp2, output[i]);
+		    }
+		    else if (format_flag == FORMAT_matrix)
+		    {
+				    fprintf(file, "%f%s", output[i], i < output.size() - 1 ? " " : "");
+		    }
+		    else if (format_flag == FORMAT_csv)
+		    {
+				    fprintf(file, "%f%s", output[i], i < output.size() - 1 ? "," : "");
+		    }
+        }
 		fprintf(file, "\n");
 	}
+//	fclose(file);
 
-	fclose( file );
-
+	delete[] in;
 	delete input;
 	delete cq;
-	delete[] in;
-	delete[] spec;
 	return 0;
 }
 
